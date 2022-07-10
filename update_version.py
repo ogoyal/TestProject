9691924ff8ee809d785e91c980f4fc91620e1b26
ogoyal <ojasgoyal@gmail.com>
2022-07-10 15:42:01 -0700

    Third commit

--- update_version.py
+++ update_version.py
@@ -9,13 +9,13 @@ def get_file_directory(file):
     if dir in ["A", "B", "C", "D", "E"]:
         return dir
 
-def update_version_file(file, headcommit):
+def update_version_file(file, commit_dict):
     with open(file, "r+") as jsonFile:
         data = json.load(jsonFile)
-        if data['commit'] != headcommit.__str__():
-            data['commit'] = headcommit.__str__()
-            data['date'] = time.strftime("%B %d, %Y", time.gmtime(
-            headcommit.committed_date))
+        headcommit, timestamp = commit_dict['headcommit'], commit_dict['timestamp']
+        if data['commit'] != headcommit:
+            data['commit'] = headcommit
+            data['date'] = timestamp
             data['version'] += 1
             jsonFile.seek(0)
             json.dump(data, jsonFile)
@@ -34,10 +34,15 @@ def main():
     for file in modified_or_new:
         dir = get_file_directory(file)
         if dir:
-            headcommit = mygit.rev_list("HEAD", dir)
-            update_version_file(f"{dir}/version.json", headcommit)
+            headcommit = mygit.rev_list("HEAD", dir).split()[-1]
+            timestamp = mygit.show("-s", "--format=%cd", "--date=format:%B %d, %Y", headcommit).split("\n")[-1]
+            print(headcommit, timestamp)
+            commit_dict = {"headcommit": headcommit, "timestamp": timestamp}
+            update_version_file(f"{dir}/version.json", commit_dict)
         else:
             headcommit = repo.head.commit
-            update_version_file(f"version.json", headcommit)
+            timestamp = time.strftime("%B %d, %Y", time.gmtime(headcommit.committed_date))
+            commit_dict = {"headcommit": headcommit.__str__(), "timestamp": timestamp}
+            update_version_file(f"version.json", commit_dict)
 
 main()
